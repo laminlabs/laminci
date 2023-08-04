869cfdd5a479902096dbb8a3309a94a102d79836
Alex Wolf <f.alexander.wolf@gmail.com>
2023-08-04 19:00:40 +0200

    📝 Account for blog

--- laminci/_docs.py
+++ laminci/_docs.py
@@ -4,6 +4,15 @@ import shutil
 from ._env import load_project_yaml
 
 
+def move_built_docs_to_slash_project_slug():
+    if os.environ["GITHUB_EVENT_NAME"] != "push":
+        return
+    yaml = load_project_yaml()
+    shutil.move("_build/html", "_build/html_tmp")
+    os.makedirs("_build/html")
+    shutil.move("_build/html_tmp", f"_build/html/{yaml['project_slug']}")
+
+
 def move_built_docs_to_docs_slash_project_slug():
     if os.environ["GITHUB_EVENT_NAME"] != "push":
         return
