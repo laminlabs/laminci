36f41b8245a796f463e662499eedb498842b504d
Alex Wolf <f.alexander.wolf@gmail.com>
2024-01-06 14:29:58 +0100

    💚 Fix

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,5 +1,5 @@
-from laminci.nox import login_testuser1
+from laminci.nox import _login_lamin_user
 
 
 def test_login_testuser1():
-    login_testuser1()
+    _login_lamin_user("testuser1@lamin.ai")
