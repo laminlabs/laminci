1014b3c33d797d330b61fe61f4f38bba4cb46d8b
Lukas Heumos <lukas.heumos@posteo.net>
2024-03-25 03:41:27 -0700

    UV for docs building (#34)
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- laminci/nox.py
+++ laminci/nox.py
@@ -68,7 +68,7 @@ def run_pytest(session: Session, coverage: bool = True, env: Optional[Dict] = No
 def build_docs(session: Session, strict: bool = False, strip_prefix: bool = False):
     prefix = "." if Path("./lndocs").exists() else ".."
     if nox.options.default_venv_backend == "none":
-        session.run(*f"pip install {prefix}/lndocs".split())
+        session.run(*f"uv pip install --system {prefix}/lndocs".split())
     else:
         session.install(f"{prefix}/lndocs")
     # do not simply add instance creation here
