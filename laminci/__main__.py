db1c91421264ac19706cba6737f7214e85d5b5f4
Alex Wolf <f.alexander.wolf@gmail.com>
2024-07-24 10:56:19 +0200

    🐛 Fix publishing to laminhub-public

--- laminci/__main__.py
+++ laminci/__main__.py
@@ -2,6 +2,7 @@ import argparse
 import importlib
 import json
 import os
+import re
 import subprocess
 from pathlib import Path
 from subprocess import PIPE, run
@@ -27,6 +28,21 @@ subparsers.add_parser(
     "doc-changes",
     help="Write latest changes",
 )
+
+
+def update_readme_version(file_path, new_version):
+    # Read the content of the file
+    with open(file_path, "r") as file:
+        content = file.read()
+
+    # Use regex to find and replace the version
+    updated_content = re.sub(
+        r"Version: `[0-9.]+`", f"Version: `{new_version}`", content
+    )
+
+    # Write the updated content back to the file
+    with open(file_path, "w") as file:
+        file.write(updated_content)
 
 
 def get_last_version_from_tags():
@@ -59,12 +75,14 @@ def publish_github_release(
     body: str = "",
     draft: bool = False,
     generate_release_notes: bool = True,
+    cwd: str | None | Path = None,
 ):
     version = parse(version)
 
     try:
-        subprocess.run(["gh", "--version"], check=True, stdout=subprocess.PIPE)
-
+        cwd = Path.cwd() if cwd is None else Path(cwd)
+        assert repo_name.split("/")[1] == cwd.name
+        subprocess.run(["gh", "--version"], check=True, stdout=subprocess.PIPE, cwd=cwd)
         try:
             command = [
                 "gh",
@@ -75,13 +93,14 @@ def publish_github_release(
                 release_name,
                 "--notes",
                 body,
-                "--generate-notes",
             ]
+            if generate_release_notes:
+                command.append("--generate-notes")
             if version.is_prerelease:
                 command.append("--prerelease")
 
-            print(f"\nrun: {command}")
-            subprocess.run(command, check=True, stdout=subprocess.PIPE)
+            print(f"\nrun: {''.join(command)}")
+            subprocess.run(command, check=True, stdout=subprocess.PIPE, cwd=cwd)
         except (subprocess.CalledProcessError, FileNotFoundError) as e:
             raise SystemExit(f"Error creating GitHub release using `gh`: {e}")
     except (subprocess.CalledProcessError, FileNotFoundError):
@@ -123,10 +142,10 @@ def main():
         # from importlib.metadata import version as get_version
         # version = get_version(package_name)
         is_laminhub = False
+        previous_version = get_last_version_from_tags()
         if package_name is not None:
             module = importlib.import_module(package_name, package=".")
             version = module.__version__
-            previous_version = get_last_version_from_tags()
             validate_version(version)
             if parse(version) <= parse(previous_version):
                 raise SystemExit(
@@ -135,8 +154,12 @@ def main():
                 )
         else:
             assert Path.cwd().name == "laminhub"
+            if not (Path.cwd().parent / "laminhub-public").exists():
+                raise ValueError(
+                    "Please clone the laminhub-public repository into the same parent"
+                    " directory as laminhub."
+                )
             is_laminhub = True
-            previous_version = "?"
             with open("ui/package.json", "r") as file:
                 version = json.load(file)["version"]
 
@@ -160,12 +183,19 @@ def main():
             repo_name=f"laminlabs/{package_name}",
             version=version,
             release_name=f"Release {version}",
+            body="See https://docs.lamin.ai/changelog",
         )
         if is_laminhub:
+            for command in commands:
+                print(f"\nrun: {command}")
+                update_readme_version("../laminhub-public/README.md", version)
+                run(command, shell=True, cwd="../laminhub-public")
             publish_github_release(
                 repo_name="laminlabs/laminhub-public",
                 version=version,
+                body="See https://docs.lamin.ai/changelog",
                 release_name=f"Release {version}",
+                generate_release_notes=False,
             )
 
         if args.pypi:
