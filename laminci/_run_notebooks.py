385c67570c078ca7311624e880b9c4e77382ddd9
Alex Wolf <f.alexander.wolf@gmail.com>
2024-09-07 16:12:32 +0200

    💚 Fix

--- laminci/_run_notebooks.py
+++ laminci/_run_notebooks.py
@@ -1,4 +1,9 @@
-def run_notebooks(file_or_folder):
+from __future__ import annotations
+
+from pathlib import Path
+
+
+def run_notebooks(file_or_folder: str | Path):
     import nbproject_test
 
-    nbproject_test.execute_notebooks(file_or_folder, write=True)
+    nbproject_test.execute_notebooks(Path(file_or_folder), write=True)
