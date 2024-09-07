34c93e19e2c1d12f63767bfd8136bbd4bb989b64
Alex Wolf <f.alexander.wolf@gmail.com>
2024-09-07 16:22:46 +0200

    💚 Fix

--- laminci/_run_notebooks.py
+++ laminci/_run_notebooks.py
@@ -6,4 +6,6 @@ from pathlib import Path
 def run_notebooks(file_or_folder: str | Path):
     import nbproject_test
 
-    nbproject_test.execute_notebooks(Path(file_or_folder), write=True)
+    path = Path(file_or_folder)
+    assert path.exists()
+    nbproject_test.execute_notebooks(path.resolve(), write=True)
