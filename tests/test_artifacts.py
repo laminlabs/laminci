389c74dda70582845fa73a6eabcfe51cb878fece
Koncopd <koncopd@gmail.com>
2025-08-07 10:24:34 +0200

    fix tests

--- tests/test_artifacts.py
+++ tests/test_artifacts.py
@@ -1,5 +1,5 @@
-from laminci._docs_artifacts import upload_docs_artifact_aws
+from laminci._docs_artifacts import upload_docs_artifact
 
 
 def test_upload_docs_artifact_aws():
-    upload_docs_artifact_aws()
+    upload_docs_artifact()
