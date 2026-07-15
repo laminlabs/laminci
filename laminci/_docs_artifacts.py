9a6922f28714a35cd5c77ddb29cd34ca3851101e
Alex Wolf <f.alexander.wolf@gmail.com>
2026-07-15 18:26:42 +0200

    💄 Prettify bash cells (#59)

--- laminci/_docs_artifacts.py
+++ laminci/_docs_artifacts.py
@@ -47,6 +47,7 @@ def process_markdown_file(input_file: str, output_file: str):
     content = open(input_file).read()
     content = re.sub(r"^---\n.*?\n---\n?", "", content, flags=re.DOTALL)
     content = re.sub(r"```python\n", r'```python tags=["hide-output"]\n', content)
+    content = re.sub(r"```bash\n", r'```bash tags=["hide-output"]\n', content)
     if re.search(r"^# ", content, flags=re.MULTILINE):
         content = re.sub(
             r"^(# .*)$", r"\1 " + badge, content, count=1, flags=re.MULTILINE
