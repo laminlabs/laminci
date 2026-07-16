db566ad61398d5b6a1bc4123d68a9329b07434fd
Alex Wolf <f.alexander.wolf@gmail.com>
2026-07-16 07:24:20 +0200

    ♻️ Move bash Jupyter magic stripping to lndocs (#60)

--- laminci/_run_notebooks.py
+++ laminci/_run_notebooks.py
@@ -9,23 +9,3 @@ def run_notebooks(file_or_folder: str | Path):
     path = Path(file_or_folder)
     assert path.exists()
     nbproject_test.execute_notebooks(path.resolve(), write=True)
-
-    import nbformat
-
-    def strip_bash(nb_path):
-        nb = nbformat.read(nb_path, as_version=4)
-        modified = False
-        for cell in nb.cells:
-            if cell.cell_type == "code" and cell.source.startswith("%%bash"):
-                import re
-
-                cell.source = re.sub(r"^%%bash\n?", "", cell.source)
-                modified = True
-        if modified:
-            nbformat.write(nb, nb_path)
-
-    if path.is_dir():
-        for nb_path in path.glob("**/*.ipynb"):
-            strip_bash(nb_path)
-    elif path.suffix == ".ipynb":
-        strip_bash(path)
